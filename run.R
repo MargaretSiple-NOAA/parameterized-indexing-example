797dfd6b8302e0b1e3dea3ec46e5656adf0e8f1a
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-11-04 11:40:36 -0700

    initial commit

---
+++ run.R
@@ -0,0 +1,20 @@
+
+# Render the intro material
+rmarkdown::render("markdown/IntroSections.Rmd",
+                  output_file = "IntroSections.docx"
+)
+
+# Render a few chapters in a loop, which I intend to insert into the doc from above:
+
+cyl.vec <- c(4,6)
+
+for (i in 1:length(cyl.vec)){
+  rmarkdown::render("markdown/ParameterizedChapter.Rmd",
+                    params = list(
+                      cylcode = cyl.vec[i]
+                    ), output_file = paste0(
+                      "ParameterizedChapter_",
+                      cyl.vec[i],
+                      "cylinders.docx")
+                    )
+}
